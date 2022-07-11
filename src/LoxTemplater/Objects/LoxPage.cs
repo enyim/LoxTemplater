38351faeb9c6393a15a26fe998621463600ec318
Attila Kisko <attila_kisko@epam.com>
2022-07-11 22:02:25 +0200

    Add LoxText for debugging

--- src/LoxTemplater/Objects/LoxPage.cs
+++ src/LoxTemplater/Objects/LoxPage.cs
@@ -3,10 +3,4 @@
 [LoxType("Page")]
 public class LoxPage : LoxHaveMeta
 {
-    //public void Add(LoxVisualObject value)
-    //{
-    //    ArgumentNullException.ThrowIfNull(Element);
-
-    //    Element.Add(value.Element);
-    //}
 }
---
+++ src/LoxTemplater/Objects/LoxText.cs
@@ -0,0 +1,18 @@
+﻿namespace Enyim.LoxTempl;
+
+[LoxType(ItemType)]
+public class LoxText : LoxVisualObject
+{
+    public const string ItemType = "Text";
+
+    public LoxText()
+    {
+        Type = ItemType;
+    }
+
+    [FromAttribute("Text")]
+    public string Text { get; set; }
+
+    [FromAttribute("Td")]
+    public bool IsTodo { get; set; }
+}
